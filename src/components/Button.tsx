5b457d43572c94a6552d2278f8575fdc6dd3d6ef
Isaac Novaes <isaac.novaes@hotmail.com>
2025-07-29 20:12:03 +0200

    Add Button primary-black and primary-transparent variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,27 @@
 import type { ComponentPropsWithoutRef } from 'react';
 import linkIcon from '../assets/icons/icon-arrow-right.svg';
 
+type Variant = 'primary' | 'primary-black' | 'primary-transparent' | 'secondary' | 'link';
+
 interface ButtonProps extends Omit<ComponentPropsWithoutRef<'button'>, 'className'> {
     readonly label: string;
-    readonly variant?: 'primary' | 'secondary' | 'link';
+    readonly variant?: Variant;
 }
 
-const classNames = {
+const buttonClassNames = {
     primary: 'bg-dark-orange hover:bg-light-orange text-white',
+    'primary-black': 'bg-black hover:bg-[#979797] text-white',
+    'primary-transparent': 'bg-transparent border-1 border-black hover:bg-black text-white',
     secondary: 'bg-white border-1 text-black border-black hover:text-white hover:bg-black',
     link: 'text-black/[50%] flex items-center justify-between gap-x-4 hover:text-dark-orange',
-} as const;
+} as const satisfies Record<Variant, string>;
 
 const Button = (props: ButtonProps) => {
     const { type, label, variant = 'primary', ...propsRest } = props;
 
     return (
         <button
-            className={`cursor-pointer px-8 py-4 text-[13px] font-bold tracking-[1px] uppercase transition-colors duration-300 ${classNames[variant]}`}
+            className={`relative z-10 cursor-pointer px-8 py-4 text-[13px] font-bold tracking-[1px] uppercase transition-colors duration-300 ${buttonClassNames[variant]}`}
             // eslint-disable-next-line react/button-has-type
             type={type ?? 'button'}
             {...propsRest}
