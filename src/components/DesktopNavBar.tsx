cc30dab3ad61e0a449b4b35c2e9af878dcfd4517
Isaac Novaes <isaac.novaes@hotmail.com>
2025-07-29 20:13:04 +0200

    Refactor DesktopNavBar with tanstack router linkOptions

--- src/components/DesktopNavBar.tsx
+++ src/components/DesktopNavBar.tsx
@@ -1,53 +1,46 @@
-import { Link } from '@tanstack/react-router';
+import { Link, linkOptions } from '@tanstack/react-router';
 import { CategoryTypeSchema } from '../types/global';
+
+const navLinks = linkOptions([
+    {
+        to: '/home',
+        label: 'home',
+        params: {},
+    },
+    {
+        to: '/category/$type',
+        params: { type: CategoryTypeSchema.Values.headphones },
+        label: 'headphones',
+    },
+    {
+        to: '/category/$type',
+        params: { type: CategoryTypeSchema.Values.speakers },
+        label: 'speakers',
+    },
+    {
+        to: '/category/$type',
+        params: { type: CategoryTypeSchema.Values.earphones },
+        label: 'earphones',
+    },
+]);
 
 const DesktopNavBar = () => {
     return (
         <nav className='desktop:block hidden'>
             <ul className='flex items-center gap-x-9'>
-                <li>
-                    <Link
-                        activeProps={{ className: 'text-dark-orange' }}
-                        className='text-sub-title hover:text-light-orange/70 tracking-[2px] uppercase transition-colors duration-300'
-                        inactiveProps={{ className: 'text-white' }}
-                        to='/home'
-                    >
-                        home
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        activeProps={{ className: 'text-dark-orange' }}
-                        className='text-sub-title hover:text-light-orange/70 tracking-[2px] uppercase transition-colors duration-300'
-                        inactiveProps={{ className: 'text-white' }}
-                        params={{ type: CategoryTypeSchema.Values.headphones }}
-                        to='/category/$type'
-                    >
-                        headphones
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        activeProps={{ className: 'text-dark-orange' }}
-                        className='text-sub-title hover:text-light-orange/70 tracking-[2px] uppercase transition-colors duration-300'
-                        inactiveProps={{ className: 'text-white' }}
-                        params={{ type: CategoryTypeSchema.Values.speakers }}
-                        to='/category/$type'
-                    >
-                        speakers
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        activeProps={{ className: 'text-dark-orange' }}
-                        className='text-sub-title hover:text-light-orange/70 tracking-[2px] uppercase transition-colors duration-300'
-                        inactiveProps={{ className: 'text-white' }}
-                        params={{ type: CategoryTypeSchema.Values.earphones }}
-                        to='/category/$type'
-                    >
-                        earphones
-                    </Link>
-                </li>
+                {navLinks.map((l) => (
+                    <li key={l.label}>
+                        <Link
+                            activeProps={{ className: 'text-dark-orange' }}
+                            className='text-sub-title hover:text-light-orange/70 tracking-[2px] uppercase transition-colors duration-300'
+                            inactiveProps={{ className: 'text-white' }}
+                            params={l.params}
+                            to={l.to}
+                        >
+                            {l.label}
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </nav>
     );
