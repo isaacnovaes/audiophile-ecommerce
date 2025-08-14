c5854d981c1d444a992d0c0b336e9281beee9aaf
Isaac Novaes <isaac.novaes@hotmail.com>
2025-08-14 13:26:51 +0200

    Finish you may also like section from product detail

--- src/routes/product-detail.$slug.tsx
+++ src/routes/product-detail.$slug.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, ErrorComponent, useRouter } from '@tanstack/react-router';
+import { createFileRoute, ErrorComponent, Link, useRouter } from '@tanstack/react-router';
 import AppContainer from '../components/AppContainer';
 import Button from '../components/Button';
 import CompanyInfo from '../components/CompanyInfo';
@@ -108,7 +108,7 @@ function RouteComponent() {
                     </ul>
                 </div>
             </div>
-            <div className='tablet:grid-cols-2 tablet:grid-rows-2 tablet:gap-x-4.5 desktop:grid-cols-[445px_1fr] grid gap-y-5'>
+            <div className='tablet:grid-cols-2 tablet:grid-rows-2 tablet:gap-x-4.5 desktop:grid-cols-[445px_1fr] desktop:mb-40 mb-30 grid gap-y-5'>
                 <picture>
                     <source
                         media={`(min-width: ${BREAKPOINTS.desktop})`}
@@ -166,6 +166,43 @@ function RouteComponent() {
                         className='tablet:h-full tablet:object-cover rounded-lg'
                     />
                 </picture>
+            </div>
+            <div className='mb-30'>
+                <h5 className='text-h5 mb-10 text-center tracking-[0.86px] uppercase'>
+                    You also may like
+                </h5>
+                <div className='tablet:flex-row tablet:gap-y-0 tablet:gap-x-5 desktop:gap-x-[30px] flex flex-col justify-between gap-y-14'>
+                    {product.others.map((other) => (
+                        <Link
+                            key={other.slug}
+                            className='tablet:gap-y-10 tablet:gap-x-8 group flex flex-col items-center justify-between gap-y-8'
+                            params={{ slug: other.slug }}
+                            to='/product-detail/$slug'
+                        >
+                            <picture>
+                                <source
+                                    media={`(min-width: ${BREAKPOINTS.desktop})`}
+                                    srcSet={other.image.desktop}
+                                />
+                                <source
+                                    media={`(min-width: ${BREAKPOINTS.tablet})`}
+                                    srcSet={other.image.tablet}
+                                />
+                                <source
+                                    media={`(max-width: ${BREAKPOINTS.tablet})`}
+                                    srcSet={other.image.mobile}
+                                />
+                                <img
+                                    alt={other.name}
+                                    className='rounded-lg'
+                                    src={other.image.mobile}
+                                />
+                            </picture>
+                            <h6 className='text-h5 leading-none uppercase'>{other.name}</h6>
+                            <Button label='see product' />
+                        </Link>
+                    ))}
+                </div>
             </div>
             <GeneralProductCategoriesList />
             <CompanyInfo />
