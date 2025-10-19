b5ca7abe6f1320cec1d9f95ab4b2a0169b526f4e
Isaac Novaes <isaac.novaes@hotmail.com>
2025-10-19 22:23:53 +0200

    Create FormRadioInput component

---
+++ src/components/FormRadioInput.tsx
@@ -0,0 +1,28 @@
+import { useId } from 'react';
+import type { UseFormRegisterReturn } from 'react-hook-form';
+
+interface Props extends UseFormRegisterReturn {
+    readonly label: string;
+    readonly value: string;
+}
+
+const FormRadioInput = (props: Props) => {
+    const { label, value, ...registerProps } = props;
+    const id = useId();
+    return (
+        <label
+            className='radio-input has-checked:border-dark-orange hover:border-dark-orange flex items-center gap-x-4 rounded-lg border-1 border-gray-100 px-6 py-4.5 text-[12px] font-bold tracking-[-0.21px] transition-colors not-last:mb-4 hover:cursor-pointer'
+            htmlFor={id}
+        >
+            <input
+                className='checked:ring-dark-orange size-5 appearance-none rounded-full border-1 border-gray-100 ring-5 ring-transparent ring-offset-4 transition-shadow ring-inset'
+                id={id}
+                type='radio'
+                value={value}
+                {...registerProps}
+            />
+            <span>{label}</span>
+        </label>
+    );
+};
+export default FormRadioInput;
