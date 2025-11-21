6637b87cc64153662db54d51f95641ab5c715c76
Isaac Novaes <isaac.novaes@hotmail.com>
2025-11-21 21:11:59 +0100

    Add hover color transition for go back button

--- src/components/GoBackButton.tsx
+++ src/components/GoBackButton.tsx
@@ -5,7 +5,7 @@ const GoBackButton = () => {
 
     return (
         <button
-            className='text-body desktop:mt-20 desktop:mb-14 mt-8 mb-6 -translate-x-3 p-3 text-black/50 hover:cursor-pointer'
+            className='text-body desktop:mt-20 desktop:mb-14 hover:text-dark-orange mt-8 mb-6 -translate-x-3 p-3 text-black/50 transition-colors hover:cursor-pointer'
             type='button'
             onClick={() => {
                 router.history.back();
